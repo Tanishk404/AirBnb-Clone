e2dfd24d02db5f62b8c4302cf4e1fec5ccf9478f
Tanishk405 <your-email@example.com>
2025-11-04 16:27:39 +0530

    Updated Google OAuth callback URL for production and local environments

--- index.js
+++ index.js
@@ -107,7 +107,6 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "https://aware-nourishment-production.up.railway.app/auth/google/callback"
-
     }, async(accessToken, refreshToken, profile, done) => {
         try {
 
